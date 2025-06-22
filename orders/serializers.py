c837e354e1c946a9c2fc79f5ab35d460b043958d
Кыялбек Темирбеков <28112002akn@gmail.com>
2025-06-22 21:32:17 +0600

    added tax, delivery, discount

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
+from decimal import Decimal
 from .models import Order, OrderItem, Product, Client
+import logging
+
+log = logging.getLogger(__name__)
+
 
 class OrderItemCreateSerializer(serializers.Serializer):
     product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
@@ -19,7 +24,7 @@ class OrderSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         items_data = validated_data.pop('items')
         client = Client.objects.get(id=1) #self.context['request'].user
-        total_sum = 0
+        subtotal = Decimal('0.00')
         validated_items = []
 
         for item_data in items_data:
@@ -33,8 +38,26 @@ class OrderSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError(f"Not enough stock for product '{product.name}'.")
 
             item_total = product.price * quantity
-            total_sum += item_total
+            subtotal += item_total
             validated_items.append((product, quantity))
+
+         # Скидка по клиенту
+        client_discount = getattr(client, 'discount_percent', 0)
+        discount = subtotal * Decimal(client_discount) / 100
+
+        # Глобальная скидка, если сумма больше 150000
+        if subtotal >= 150000:
+            discount += subtotal * Decimal('0.10')
+
+        # НДС 12%
+        tax = (subtotal - discount) * Decimal('0.12')
+
+        # Доставка: бесплатно, если > 2000
+        delivery = Decimal('0.00') if subtotal >= 2000 else Decimal('300.00')
+
+        # Итог
+        total_sum = subtotal - discount + tax + delivery
+        log.info(f"Subtotal '{subtotal}', Total '{total_sum}'")
 
         order = Order.objects.create(client=client, total_sum=total_sum, **validated_data)
 
